3c8b6bfa8bd711503a4d89a2d32d2921fc687b72
Tobbemane <tobias.erixon@edu.nykoping.se>
2021-05-23 12:06:45 +0200

    klar/final product

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 'use strict'
 
+if (document.getElementById||document.all)
+var crossheader=document.getElementById? document.getElementById("flyin").style : document.all.flyin.style
 
+var start = 0;
+
+function animatein(){
+if (parseInt(crossheader.left)<0)
+crossheader.left=parseInt(crossheader.left)+20+"px"
+else{
+crossheader.left=0
+crossheader.fontStyle="normal"
+clearInterval(start)
+}
+}
+
+if (document.getElementById||document.all)
+start=setInterval("animatein()",10)
